7768190b5d43cba9423654d68f0aa01b29e10f25
Frank Rügamer <frankruegamer@gmail.com>
2023-12-02 16:42:44 +0100

    Add code for day1 part1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,23 @@
+static INPUT: &str = include_str!("../input");
+
 fn main() {
-    println!("Hello, world!");
+    let sum_of_all_lines: u32 = INPUT.lines().map(get_number).sum();
+    println!("Result: {}", sum_of_all_lines)
+}
+
+fn get_number(line: &str) -> u32 {
+    let mut first_number = None;
+    let mut last_number = None;
+    for c in line.chars() {
+        if c.is_numeric() {
+            if first_number.is_none() {
+                first_number = Some(c);
+            }
+            last_number = Some(c);
+        }
+    }
+    let first_number = first_number.expect("could not parse number from line");
+    let last_number = last_number.unwrap();
+    let combined_number = format!("{}{}", first_number, last_number);
+    combined_number.parse().unwrap_or_else(|_| panic!("could not parse {} into a number", combined_number))
 }
