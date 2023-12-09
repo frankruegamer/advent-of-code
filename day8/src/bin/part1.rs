7f9c65e78a4bd89ed6605fd0205c9ca2e09a11c9
Frank Rügamer <frankruegamer@gmail.com>
2023-12-10 00:29:49 +0100

    Extract day 8 lib

--- day8/src/bin/part1.rs
+++ day8/src/bin/part1.rs
@@ -1,76 +1,7 @@
+use day8::{Direction, Directions, Fork};
 use std::collections::HashMap;
-use std::iter::{Chain, Cycle};
-use std::slice::Iter;
-use std::str::FromStr;
 
 const INPUT: &str = include_str!("../../input");
-
-#[derive(Debug)]
-struct Directions {
-    directions: Vec<Direction>,
-}
-
-#[derive(Debug)]
-struct ParseMovesErr;
-
-impl FromStr for Directions {
-    type Err = ParseMovesErr;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let directions = s
-            .chars()
-            .map(|s| s.to_string().parse::<Direction>())
-            .collect::<Result<_, _>>()
-            .map_err(|_| ParseMovesErr)?;
-        Ok(Self { directions })
-    }
-}
-
-impl Directions {
-    fn moves_iter(&self) -> Cycle<Iter<'_, Direction>> {
-        self.directions.iter().cycle()
-    }
-}
-
-#[derive(Debug)]
-enum Direction {
-    Left,
-    Right,
-}
-
-impl FromStr for Direction {
-    type Err = ParseMovesErr;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let direction = match s {
-            "L" => Direction::Left,
-            "R" => Direction::Right,
-            _ => Err(ParseMovesErr)?,
-        };
-        Ok(direction)
-    }
-}
-
-#[derive(Debug)]
-struct Fork {
-    name: String,
-    left: String,
-    right: String,
-}
-
-impl FromStr for Fork {
-    type Err = ParseMovesErr;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut string = s.to_owned();
-        string.retain(|s| !['=', '(', ',', ')'].contains(&s));
-        let mut parts = string.split_whitespace();
-        let name = parts.next().ok_or(ParseMovesErr)?.to_owned();
-        let left = parts.next().ok_or(ParseMovesErr)?.to_owned();
-        let right = parts.next().ok_or(ParseMovesErr)?.to_owned();
-        Ok(Fork { name, left, right })
-    }
-}
 
 fn main() {
     let option = INPUT.split_once("\n\n");
---
+++ day8/src/lib.rs
@@ -0,0 +1,70 @@
+use std::iter::Cycle;
+use std::slice::Iter;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Directions {
+    directions: Vec<Direction>,
+}
+
+#[derive(Debug)]
+pub struct ParseMovesErr;
+
+impl FromStr for Directions {
+    type Err = ParseMovesErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let directions = s
+            .chars()
+            .map(|s| s.to_string().parse::<Direction>())
+            .collect::<Result<_, _>>()
+            .map_err(|_| ParseMovesErr)?;
+        Ok(Self { directions })
+    }
+}
+
+impl Directions {
+    pub fn moves_iter(&self) -> Cycle<Iter<'_, Direction>> {
+        self.directions.iter().cycle()
+    }
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+impl FromStr for Direction {
+    type Err = ParseMovesErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let direction = match s {
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => Err(ParseMovesErr)?,
+        };
+        Ok(direction)
+    }
+}
+
+#[derive(Debug)]
+pub struct Fork {
+    pub name: String,
+    pub left: String,
+    pub right: String,
+}
+
+impl FromStr for Fork {
+    type Err = ParseMovesErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut string = s.to_owned();
+        string.retain(|s| !['=', '(', ',', ')'].contains(&s));
+        let mut parts = string.split_whitespace();
+        let name = parts.next().ok_or(ParseMovesErr)?.to_owned();
+        let left = parts.next().ok_or(ParseMovesErr)?.to_owned();
+        let right = parts.next().ok_or(ParseMovesErr)?.to_owned();
+        Ok(Fork { name, left, right })
+    }
+}
