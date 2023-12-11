867af977fb8e659fb625e167f10c60264d615097
Frank Rügamer <frankruegamer@gmail.com>
2023-12-11 22:40:28 +0100

    Solve day 10 part 1

--- day10/src/bin/part1.rs
+++ day10/src/bin/part1.rs
@@ -1,29 +1,35 @@
 use std::ops::Add;
 
-const INPUT: &str = include_str!("../../input2");
-const SIZE: usize = 5;
+const INPUT: &str = include_str!("../../input");
+const SIZE: usize = 140;
 type Grid = [[char; SIZE]; SIZE];
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Position {
     pos: (usize, usize),
     symbol: Symbol,
 }
 
+impl PartialEq for Position {
+    fn eq(&self, other: &Self) -> bool {
+        self.pos == other.pos
+    }
+}
+
 impl Add<Direction> for &Position {
-    type Output = (usize, usize);
+    type Output = Option<(usize, usize)>;
 
     fn add(self, rhs: Direction) -> Self::Output {
         let (current_x, current_y) = self.pos;
         let (direction_x, direction_y) = rhs.vector();
-        (
-            current_x.checked_add_signed(direction_x).unwrap(),
-            current_y.checked_add_signed(direction_y).unwrap(),
-        )
+        Some((
+            current_x.checked_add_signed(direction_x)?,
+            current_y.checked_add_signed(direction_y)?,
+        ))
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 enum Symbol {
     Nothing,
     NorthSouth,
@@ -52,19 +58,6 @@ impl From<char> for Symbol {
 }
 
 impl Symbol {
-    fn string_repr(&self) -> char {
-        match self {
-            Symbol::Nothing => '.',
-            Symbol::NorthSouth => '|',
-            Symbol::EastWest => '-',
-            Symbol::NorthEast => 'L',
-            Symbol::NorthWest => 'J',
-            Symbol::SouthWest => '7',
-            Symbol::SouthEast => 'F',
-            Symbol::StartingPosition => 'S',
-        }
-    }
-
     fn connections(&self) -> Option<(Direction, Direction)> {
         match self {
             Symbol::Nothing => None,
@@ -113,15 +106,31 @@ fn main() {
         .collect::<Vec<[char; SIZE]>>()
         .try_into()
         .unwrap();
+
+    let mut counter: usize = 0;
     let starting_pos = find_starting_pos(&grid);
-    println!("{:?}", starting_pos);
-    let places = connecting_places(&grid, &starting_pos);
-    println!("{:?}", places);
+    let mut previous_pos = starting_pos.clone();
+    let (mut current_pos, _) = connecting_places(&grid, &starting_pos);
+
+    while current_pos != starting_pos {
+        let (connection1, connection2) = connecting_places(&grid, &current_pos);
+        let next_position = vec![connection1, connection2]
+            .into_iter()
+            .find(|con| *con != previous_pos)
+            .unwrap();
+        previous_pos = current_pos;
+        current_pos = next_position;
+        counter += 1;
+    }
+
+    let result = (counter + 1).div_ceil(2);
+    println!("Result {}", result);
 }
 
 fn find_starting_pos(grid: &Grid) -> Position {
-    for (x, row) in grid.iter().enumerate() {
-        for (y, char) in row.iter().enumerate() {
+    // important y is the other value
+    for (y, row) in grid.iter().enumerate() {
+        for (x, char) in row.iter().enumerate() {
             if Symbol::from(*char) == Symbol::StartingPosition {
                 return Position {
                     pos: (x, y),
@@ -143,18 +152,20 @@ fn connecting_places(grid: &Grid, pos: &Position) -> (Position, Position) {
         .connections()
         .unwrap_or_else(|| panic!("unexpected symbol at this point {:?}", pos));
 
-    let first = position_in_direction(grid, pos, first_connection);
-    let second = position_in_direction(grid, pos, second_connection);
+    let first = position_in_direction(grid, pos, first_connection).unwrap();
+    let second = position_in_direction(grid, pos, second_connection).unwrap();
     (first, second)
 }
 
 fn starting_connections(grid: &Grid, pos: &Position) -> (Position, Position) {
+    // TODO fix if S is at border
     let mut positions = Vec::new();
 
     let mut add_if_possible = |to, opposite| {
-        let position = position_in_direction(grid, pos, to);
-        if position.symbol.has_connection_to(opposite) {
-            positions.push(position);
+        if let Some(position) = position_in_direction(grid, pos, to) {
+            if position.symbol.has_connection_to(opposite) {
+                positions.push(position);
+            }
         }
     };
     add_if_possible(Direction::North, Direction::South);
@@ -163,18 +174,49 @@ fn starting_connections(grid: &Grid, pos: &Position) -> (Position, Position) {
     add_if_possible(Direction::West, Direction::East);
     if positions.len() != 2 {
         panic!(
-            "more than one direction from the starting position {:?}",
-            positions
+            "more than one direction from the starting {:?} position {:?}",
+            pos, positions
         );
     }
     (positions.swap_remove(0), positions.swap_remove(0))
 }
 
-fn position_in_direction(grid: &Grid, from_position: &Position, direction: Direction) -> Position {
-    let (x, y) = from_position + direction.clone();
+fn position_in_direction(
+    grid: &Grid,
+    from_position: &Position,
+    direction: Direction,
+) -> Option<Position> {
+    let (x, y) = (from_position + direction.clone())?;
     let symbol = grid[y][x].into();
-    Position {
+    Some(Position {
         pos: (x, y),
         symbol,
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Symbol::Nothing;
+    use crate::{Direction, Position};
+
+    #[test]
+    fn test_add_direction_to_position() {
+        let position = Position {
+            pos: (0, 0),
+            symbol: Nothing,
+        };
+        assert_eq!(&position + Direction::North, None);
+        assert_eq!(&position + Direction::East, Some((1, 0)));
+        assert_eq!(&position + Direction::South, Some((0, 1)));
+        assert_eq!(&position + Direction::West, None);
+
+        let position = Position {
+            pos: (1, 1),
+            symbol: Nothing,
+        };
+        assert_eq!(&position + Direction::North, Some((1, 0)));
+        assert_eq!(&position + Direction::East, Some((2, 1)));
+        assert_eq!(&position + Direction::South, Some((1, 2)));
+        assert_eq!(&position + Direction::West, Some((0, 1)));
     }
 }
